181e737deb8756b2aac5484890cd71bba76daa50
Arpn00798 <signalssupplements@gmail.com>
2026-03-26 13:54:14 +0100

    Add CompetitorIQ-dark.jsx React component

---
+++ CompetitorIQ-dark.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const CompetitorIQDark = () => {
+    return (
+        <div style={{ backgroundColor: 'black', color: 'white', padding: '20px' }}>
+            <h1>Competitor IQ - Dark Mode</h1>
+            <p>This is a dark mode version of the Competitor IQ component.</p>
+        </div>
+    );
+};
+
+export default CompetitorIQDark;
